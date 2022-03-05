a6008c98021251c8ab439718775d12ed25cf63ec
Grace229 <itiunggrace@gmail.com>
2022-03-05 15:07:39 +0100

    ui update

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./Jumbotron.scss";
 
 export default function Jumbotron({ children }) {
-  return <div className="jumbotron">{children}</div>;
+  return <div className="jumbotron">{children}</div>
 }
