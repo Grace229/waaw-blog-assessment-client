d0e5a7abb85299470494eb5ec074b4158dc54e2e
Grace229 <itiunggrace@gmail.com>
2022-03-05 10:57:04 +0100

    initial commit

---
+++ node_modules/@lottiefiles/react-lottie-player/dist/stories/index.stories.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="react" />
+import '@storybook/addon-console';
+declare const _default: {
+    title: string;
+    parameters: {
+        info: {
+            inline: boolean;
+        };
+    };
+};
+export default _default;
+export declare const LottiePlayer: () => JSX.Element;
---
+++ src/components/Footer/FooterComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.scss";
+
+export default function FooterComponent() {
+  return (
+    <div className="footer-component">© WAAW Blog 2022 by Grace Itiung </div>
+  );
+}
